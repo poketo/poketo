762c585e643e0ddcda7caff6da74776eb648d7fc
Ross Zurowski <ross@rosszurowski.com>
2018-08-12 18:04:30 -0700

    Update spec snapshots

--- src/adapters/shared/fool-slide.js
+++ src/adapters/shared/fool-slide.js
@@ -74,7 +74,7 @@ export default function makeFoolSlideAdapter(options: Options): SiteAdapter {
       const jsonUrl = `${normalizedBaseUrl}/api/reader/comic/stub/${seriesSlug}/format/json`;
       const json = await utils.getJSON(jsonUrl);
       const title = json.comic.name.trim();
-      const coverImageUrl = json.comic['thumb_url'];
+      const coverImageUrl = json.comic['thumb_url'] || undefined;
 
       const chapters: ChapterMetadata[] = json.chapters.map(data => {
         const subchapter =
